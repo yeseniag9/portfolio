cbf7b9a1d8455fe94125b33f69b538eb0dee46fd
Yesenia <yeseniaaag9@gmail.com>
2024-01-30 01:41:25 -0600

    pausing js work

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 111) {
-          $("nav").css("background" , "url("images/bg-dubai.jpg")");
+          $("nav").css("background" , "url('images/bg-dubai.jpg')");
         }
   
         else{
-            $(".black").css("background" , "#333");  	
+            $(".nav").css("background" , "white");  	
         }
     })
   })
