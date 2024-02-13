781a6ff8b3faecf34450ae3948c3b1400aa0a056
Yesenia <yeseniaaag9@gmail.com>
2024-02-12 22:45:52 -0600

    adjusted on scroll if page px

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 window.onscroll = () => {
     const nav = document.querySelector('#nav');
-    if(this.scrollY <= 80) nav.className = ''; else nav.className = 'scroll';
+    if(this.scrollY <= 109) nav.className = ''; else nav.className = 'scroll';
   };
