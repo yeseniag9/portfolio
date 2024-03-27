73aed607603d958fabf9b8f4b8e1be47c2838222
Yesenia <yeseniaaag9@gmail.com>
2024-03-27 10:31:25 -0500

    fixed contact link

--- main.js
+++ main.js
@@ -4,4 +4,7 @@ window.onscroll = () => {
 
   const a = document.querySelector('a')
   if(this.scrollY <= 109) a.className = ''; else a.className = 'scroll';
+
+  const i = document.querySelector('#i')
+  if(this.scrollY <= 109) i.className = ''; else i.className = 'scroll';
 }
