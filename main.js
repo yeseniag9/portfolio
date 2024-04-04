77aec8713d3b15974b9ca40cb5de67105d909111
Yesenia <yeseniaaag9@gmail.com>
2024-04-04 06:28:44 -0500

    re-arranged js file code

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ window.onscroll = () => {
   const a = document.querySelector('a')
   if(this.scrollY <= 109) a.className = ''; else a.className = 'scroll';
 
-  const i = document.querySelector('#i')
-  if(this.scrollY <= 109) i.className = ''; else i.className = 'scroll';
-
   const drop1 = document.querySelector('#drop1')
   if(this.scrollY <= 109) drop1.className = ''; else drop1.className = 'scroll';
 
   const drop2 = document.querySelector('#drop2')
   if(this.scrollY <= 109) drop2.className = ''; else drop2.className = 'scroll';
+
+  const i = document.querySelector('#i')
+  if(this.scrollY <= 109) i.className = ''; else i.className = 'scroll';
 }
