55cc3d01b41aa95c64f168688ddbe0be6b4cba66
Yesenia <yeseniaaag9@gmail.com>
2024-04-25 16:01:30 -0500

    added javascript code

--- main.js
+++ main.js
@@ -14,3 +14,30 @@ window.onscroll = () => {
   const i = document.querySelector('#i')
   if(this.scrollY <= 109) i.className = ''; else i.className = 'scroll';
 }
+
+function validateForm(){
+  let name = document.forms['contact-form']['name'].value;
+  let email = document.forms['contact-form']['email'].value;
+  let message = document.forms['contact-form']['message'].value;
+
+  if (name == '' || hasNumber(name)){
+      alert('Name must be filled out and must only contain letters');
+      return false;
+  } else if (email == ''){
+      alert('Email or phone must be filled out');
+      return false;
+  } else if (!(email.includes('@'))){
+      alert('Email does not seem to be valid');
+      return false;
+  } else if (message == ''){
+      alert('Message must be filled out');
+      return false;
+  } else {
+      // confirm or cancel
+      return confirm('Do you really want to sent the message?')
+  }
+}
+
+function hasNumber(myString) {
+  return /\d/.test(myString);
+}
