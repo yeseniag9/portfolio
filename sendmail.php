189b3fef9bfcb630a8ddcc0ac1d69efd0b7b4643
Yesenia <yeseniaaag9@gmail.com>
2024-04-25 20:57:30 -0500

    working on php mail code

---
+++ sendmail.php
@@ -0,0 +1,28 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(
+        !empty($_POST['name'])
+        && !empty($_POST['email'])
+        && !empty($_POST['message'])
+    ){
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $message = $_POST["message"];
+
+
+        $to = "yeseniag9@hotmail.com";
+        $subject = "New Contact Form Submission";
+        $body = "Name: {$name}\nEmail: {$email}\nMessage: {$message}";
+        $headers = "From: {$email}";
+
+
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Message sent successfully!";
+        } else {
+            echo "Failed to send message.";
+        }
+    }
+}
+
+?>
