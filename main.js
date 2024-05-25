b98a89ea808c5011292ccbb047bfa8b79ac07ceb
Yesenia <yeseniaaag9@gmail.com>
2024-05-25 01:12:37 -0500

    created function checkInputs

--- main.js
+++ main.js
@@ -47,8 +47,31 @@ function sendEmail() {
     );
 }
 
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        item.addEventListener("keyup", () => {
+            if (item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        });
+    }
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    checkInputs();
 
-    sendEmail();
+   // sendEmail();
 });
